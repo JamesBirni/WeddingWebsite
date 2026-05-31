66f555f85542f05dbd6787c075974cbc8719cff1
James Birnie <jkbirnie@gmail.com>
2026-05-31 15:00:55 -0700

    "base should start with slash"

--- WeddingWebiste/vite.config.js
+++ WeddingWebiste/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'process.env.VITE_BASE_PATH'||"/WeddingWebsite", // Set base to current directory for correct asset paths in production
+  base: '/process.env.VITE_BASE_PATH'||"/WeddingWebsite", // Set base to current directory for correct asset paths in production
 })
